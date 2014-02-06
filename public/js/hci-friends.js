ab35e6d734213b2a16852d6cb9be6728c6148ee7
Daniel Jih <dan.jih@gmail.com>
2014-02-06 00:02:18 +0000

    slide 28

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,14 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".nameLink").click(clickName);
+}
+
+function clickName(e) {
+	e.preventDefault();
+	var name = $(this).text();
+	var newName = anagrammedName(name);
+	$(this).text(newName);
 }
 
 function anagrammedName(name) {
